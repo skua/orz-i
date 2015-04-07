9c4f28c0d0778b8a99c102370d40571390f23289
skua <i@55u.me>
2015-04-07 14:14:36 +0800

    add weixin api

--- app.js
+++ app.js
@@ -9,8 +9,29 @@ app.use(logger());
 
 // route middleware
 var routes = require('./routes.js');
+
+
+//var sign = require('./lib/sign.js');
+
+//console.log(sign('jsapi_ticket', 'http://example.com'));
+/*
+ *something like this
+ *{
+ *  jsapi_ticket: 'jsapi_ticket',
+ *  nonceStr: '82zklqj7ycoywrk',
+ *  timestamp: '1415171822',
+ *  url: 'http://example.com',
+ *  signature: '1316ed92e0827786cfda3ae355f33760c4f70c1f'
+ *}
+ */
+
+
+
+
 app.use(route.get('/', routes.index));
 app.use(route.get('/post/list', routes.list));
+
+app.use(route.get('/weixinapi',routes.weixinapi));
  
 app.listen(3443);
 
--- routes.js
+++ routes.js
@@ -17,7 +17,9 @@ var render = require('./lib/render');
 //var posts = wrap(db.get('posts'));
 
 
+var sign = require('./lib/sign.js');
 
+//console.log(sign('jsapi_ticket', 'http://example.com'));
 
 
 
@@ -37,4 +39,18 @@ module.exports.index = function *index() {
 
 module.exports.list = function *list() {
   this.body = yield render('list');
+};
+
+module.exports.weixinapi = function *weixinapi() {
+//var wx_config={debug:false,appId:'wx5d35652f96506a51',timestamp:1428375278,nonceStr:'kXHcVKQiR7GRtdoDtx',signature: 'd17ca4ab1d97ed9d556d94039d798179260150f8'};
+  var wx_sign = sign('sM4AOVdWfPE4DxkXGEs8VF5kYx3BiX1JlFvmHFJiZSEdEU2KjnAldDihoCtf-VbVbiBZgd-WQGYO3Mi_TQVV7A', 'http://t.orz-i.com/');
+  var wx_config = "var wx_config= {debug:true," +
+                  "appId:'wx37d3b2dac43a589a',timestamp:"+
+                   wx_sign.timestamp + 
+                   ",nonceStr:'"+
+                   wx_sign.nonceStr + 
+                   "',signature: '"+
+                   wx_sign.signature + 
+                   "'};"
+  this.body = wx_config;
 };
