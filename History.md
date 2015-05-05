115c0b7bf61626f2efad165046ceb7b514f3bf74
skua <i@55u.me>
2015-05-05 11:06:39 +0800

    历史记录+cdn.orz-i.com

--- History.md
+++ History.md
@@ -1,9 +1,8 @@
 
 55u.me迁移记录
 ==================
-#orz-i 数据格式只保留wp之前重要字段
-#账户管理 评论管理以后再补充
-
+orz-i 数据格式只保留wp之前重要字段
+账户管理 评论管理以后再补充
 * wp_posts
 * ID：自增唯一ID
 * post_author：对应作者
@@ -13,10 +12,13 @@
 * post_status：文章状态（publish/auto-draft/inherit等）
 * post_modified：修改时间
 * post_url：URL地址
-* post_description：用于seo的描述
-* post_keywords：用于seo的关键词
-
+* post_description：用于seo的描述/微信描述
+* post_keywords：用于seo的关键词 
+* post_wximg：用于微信缩略图
 ==================
-
 七牛同步图片数据
 静态文件cdn.orz-i.com
+==================
+启用PM2管理服务
+改为io.js
+review文章删除了一些清理由于几次改版迁移产生的无用class和一些行内样式标签
--- routes.js
+++ routes.js
@@ -15,10 +15,10 @@ var wrap = require('co-monk');
 //testData 集合名称
 
 var db = monk('localhost/local');
- 
+
 var testData = wrap(db.get('testData'));
-  testData.find({}, function(err, docs) {
-   //console.log(docs);
+testData.find({}, function(err, docs) {
+  //console.log(docs);
 })
 
 
@@ -32,44 +32,61 @@ var sign = require('./lib/sign.js');
  */
 
 
-module.exports.index = function *index() {
+module.exports.index = function * index() {
   this.body = yield render('index');
 };
 
-module.exports.list = function *list() {
-  //this.body = yield render('list');
+module.exports.list = function * list() {
 
- var post = yield testData.findOne({post_id:"1239"});
+  var limit = 1
+  var posts = yield testData.find({});
 
-
-
+  // this.body = yield render('list', { posts: postList });
+  for (i = 0; i < posts.length; i++) {
+    if (posts[i].post_id > limit) {
+      var post = posts[i];
+      break;
+    }
+  }
   //if (!post) this.throw(404, 'invalid post id');
-  this.body = yield render('list', { post: post });
+  this.body = yield render('list', {
+    post: post
+  });
+  //this.body = yield render('list', { next:postb });
 };
 
 /**
  * Show post :id.
  */
-module.exports.show = function *show(id) {
-  var post = yield testData.findOne({post_id:id});
-  if (!post) this.throw(404, 'invalid post id');
-  this.body = yield render('list', { post: post });
+module.exports.show = function * show(id) {
+  var limit = id;
+  var posts = yield testData.find({});
+  for (i = 0; i < posts.length; i++) {
+    if (posts[i].post_id > limit) {
+      var post = posts[i];
+      break;
+    }
+  }
+  if (!post) this.
+  throw (404, 'invalid post id');
+  this.body = yield render('show', {
+    post: post
+  });
 };
-
 
 
 
 /**
  * Show creation form.
  */
-module.exports.add = function *add() {
+module.exports.add = function * add() {
   this.body = yield render('new');
 };
 
 /**
  * Create a post.
  */
-module.exports.create = function *create() {
+module.exports.create = function * create() {
   var post = yield parse(this);
   //console.log(this);
   yield testData.insert(post);
@@ -78,16 +95,16 @@ module.exports.create = function *create() {
 
 
 
-module.exports.weixinapi = function *weixinapi() {
-//var wx_config={debug:false,appId:'wx5d35652f96506a51',timestamp:1428375278,nonceStr:'kXHcVKQiR7GRtdoDtx',signature: 'd17ca4ab1d97ed9d556d94039d798179260150f8'};
+module.exports.weixinapi = function * weixinapi() {
+  //var wx_config={debug:false,appId:'wx5d35652f96506a51',timestamp:1428375278,nonceStr:'kXHcVKQiR7GRtdoDtx',signature: 'd17ca4ab1d97ed9d556d94039d798179260150f8'};
   var wx_sign = sign('sM4AOVdWfPE4DxkXGEs8VF5kYx3BiX1JlFvmHFJiZSEdEU2KjnAldDihoCtf-VbVbiBZgd-WQGYO3Mi_TQVV7A', 'http://t.orz-i.com/');
   var wx_config = "var wx_config= {debug:true," +
-                  "appId:'wx37d3b2dac43a589a',timestamp:"+
-                   wx_sign.timestamp + 
-                   ",nonceStr:'"+
-                   wx_sign.nonceStr + 
-                   "',signature: '"+
-                   wx_sign.signature + 
-                   "'};"
+    "appId:'wx37d3b2dac43a589a',timestamp:" +
+    wx_sign.timestamp +
+    ",nonceStr:'" +
+    wx_sign.nonceStr +
+    "',signature: '" +
+    wx_sign.signature +
+    "'};"
   this.body = wx_config;
 };
