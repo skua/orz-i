eb90af71ec2f6f87d6e7fcfabaab236038b9464c
skua <i@55u.me>
2015-06-29 17:07:59 +0800

    发码

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 var logger = require('koa-logger');
 var route = require('koa-route');
 var koa = require('koa');
 var app = module.exports = koa();
 
-  
+
 app.use(logger());
 
 // route middleware
@@ -14,34 +13,17 @@ var routes = require('./routes.js');
 var host = "http://55u.me";
 
 
-//var sign = require('./lib/sign.js');
-
-//console.log(sign('jsapi_ticket', 'http://example.com'));
-/*
- *something like this
- *{
- *  jsapi_ticket: 'jsapi_ticket',
- *  nonceStr: '82zklqj7ycoywrk',
- *  timestamp: '1415171822',
- *  url: 'http://example.com',
- *  signature: '1316ed92e0827786cfda3ae355f33760c4f70c1f'
- *}
- */
-
-
-
 
 app.use(route.get('/', routes.index));
-app.use(route.get('/post/list', routes.list));
-app.use(route.get('/post/new', routes.add));
-app.use(route.post('/post', routes.create));
-app.use(route.get('/post/show/:id', routes.show));
 
-app.use(route.get('/post/wx', routes.wx));
 
-//app.use(route.get('/weixinapi',routes.weixinapi));
- 
+app.use(route.get('/list', routes.list));
+
+app.use(route.get('/aly', routes.aly));
+
+app.use(route.get("/mail", routes.mail));
+
+
 app.listen(3443);
 
 console.log("连接成功！");
-
--- routes.js
+++ routes.js
@@ -9,116 +9,163 @@ var render = require('./lib/render');
 // Set up monk
 var monk = require('monk');
 var wrap = require('co-monk');
+var jsSHA = require('jssha');
+
+var https = require('https');
 
 
+var appid = "wx968f0697f984ba93";
+var secret = "4ebe9326c709b80b7df7ee4639cd1d2c";
 
-var koa = require('koa');
-var app = module.exports = koa();
+var _url = "http://io.orz-i.com";
+
 // 链接mongo
 //local 库名
 //testData 集合名称
 
 var db = monk('localhost/local');
 
-var testData = wrap(db.get('testData'));
-testData.find({}, function(err, docs) {
-  //console.log(docs);
+var test = wrap(db.get('test'));
+test.find({}, function(err, docs) {
+  console.log(docs);
 })
 
 
-var sign = require('./lib/sign.js');
 
-//console.log(sign('jsapi_ticket', 'http://example.com'));
-//console.log(sign('sM4AOVdWfPE4DxkXGEs8VJqSWLLQkyTACRLmRAJ4qPPf7fKHx_BibTiXcqIGgcCdyE47u6Dwau45ZfuNOKysMg', 'http://t.orz-i.com'));
-// And now... the route definitions
-/**
- * Post listing.
- */
+
+  // 随机字符串产生函数
+  var createNonceStr = function() {
+    return Math.random().toString(36).substr(2, 15);
+  };
+
+  // 时间戳产生函数
+  var createTimeStamp = function () {
+    return parseInt(new Date().getTime() / 1000) + '';
+  };
+
+
+    // 计算签名
+  var calcSignature = function (ticket, noncestr, ts, url) {
+    var str = 'jsapi_ticket=' + ticket + '&noncestr=' + noncestr + '&timestamp='+ ts +'&url=' + url;
+    shaObj = new jsSHA(str, 'TEXT');
+    return shaObj.getHash('SHA-1', 'HEX');
+  }
+
+
+// // 获取微信签名所需的access_token
+// https.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + secret, function(_res) {
+//   var str = '';
+//   _res.on('data', function(data) {
+//     str += data;
+//   });
+//   _res.on('end', function() {
+//     console.log('return access_token:  ' + str);
+//     try {
+//       var resp = JSON.parse(str);
+//     } catch (e) {
+//       return errorRender(res, '解析access_token返回的JSON数据错误', str);
+//     }
+
+//     getTicket(_url, resp);
+//   });
+// });
+
+
+
+// // 获取微信签名所需的ticket
+// var getTicket = function(url, accessData) {
+//   https.get('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + accessData.access_token + '&type=jsapi', function(_res) {
+//     var str = '',
+//       resp;
+//     _res.on('data', function(data) {
+//       str += data;
+//     });
+//     _res.on('end', function() {
+//       console.log('return ticket:  ' + str);
+//       try {
+//         resp = JSON.parse(str);
+//       } catch (e) {
+//         return errorRender(res, '解析远程JSON数据错误', str);
+//       }
+
+//       //var appid = appIds[index].appid;
+//       var ts = createTimeStamp();
+//       var nonceStr = createNonceStr();
+//       var ticket = resp.ticket;
+//       var signature = calcSignature(ticket, nonceStr, ts, url);
+
+
+//       console.log(signature);
+
+//       // cachedSignatures[url] = {
+//       //   nonceStr: nonceStr,
+//       //   appid: appid,
+//       //   timestamp: ts,
+//       //   signature: signature,
+//       //   url: url
+//       // };
+
+//       // responseWithJson(res, {
+//       //   nonceStr: nonceStr,
+//       //   timestamp: ts,
+//       //   appid: appid,
+//       //   signature: signature,
+//       //   url: url
+//       // });
+//     });
+//   });
+// };
 
 
 
 module.exports.index = function * index() {
   this.body = yield render('index');
+
 };
+
+
+
+module.exports.mail = function * mail() {
+
+  function randomString(len) {　　
+    len = len || 20;　　
+    var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'; /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/ 　　
+    var maxPos = $chars.length;　　
+    var pwd = '';　　
+    for (i = 0; i < len; i++) {　　　　
+      pwd += $chars.charAt(Math.floor(Math.random() * maxPos));　　
+    }　　
+    return pwd;
+  }
+  var codes = randomString(20);
+  var query = this.query;
+  var data = {}
+
+  var mailList = ["i@55u.me", "i@f2e.it"];
+  if (mailList.indexOf(query.mail) == -1) {
+    data.msg = "不符合要求";
+    this.body = data;
+  } else {
+    data.mail = query.mail;
+    data.name = query.name;
+    data.codes = codes;
+    yield test.insert(data);
+    this.body = data;
+  }
+
+};
+
+
 
 module.exports.list = function * list() {
-
-  var limit = 1
-  var posts = yield testData.find({});
-
-  // this.body = yield render('list', { posts: postList });
-  for (i = 0; i < posts.length; i++) {
-    if (posts[i].post_id > limit) {
-      var post = posts[i];
-      break;
-    }
-  }
-  //if (!post) this.throw(404, 'invalid post id');
+  var postList = yield test.find({});
   this.body = yield render('list', {
-    post: post
-  });
-  //this.body = yield render('list', { next:postb });
-};
-
-/**
- * Show post :id.
- */
-module.exports.show = function * show(id) {
-  var limit = id;
-  var posts = yield testData.find({});
-  for (i = 0; i < posts.length; i++) {
-    if (posts[i].post_id > limit) {
-      var post = posts[i];
-      break;
-    }
-  }
-
-
-
-  if (!post) this.
-  throw (404, 'invalid post id');
-  this.body = yield render('show', {
-    post: post
-
+    posts: postList
   });
 };
 
 
 
-module.exports.add = function * add() {
-  this.body = yield render('new');
+module.exports.aly = function * aly() {
+  this.body = yield render('aly');
 };
-
-/**
- * Show creation form.
- */
-module.exports.wx = function * wx() {
-  url = 'http://t.orz-i.com' + this.req.url
-  wxSign = sign('sM4AOVdWfPE4DxkXGEs8VJqSWLLQkyTACRLmRAJ4qPPf7fKHx_BibTiXcqIGgcCdyE47u6Dwau45ZfuNOKysMg', url);
-
-  this.body = yield render('wx', {
-    sign: wxSign
-  });
-
-
-  // app.get('https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx968f0697f984ba93&secret=4ebe9326c709b80b7df7ee4639cd1d2c&code=03166226e7e8eedf90f7f4b3439b161O&grant_type=authorization_code', function(req, res) {
-
-  //   console.log(req);
-
-  // });
-
-  //console.log(url);
-
-
-};
-
-/**
- * Create a post.
- */
-module.exports.create = function * create() {
-  var post = yield parse(this);
-  //console.log(this);
-  yield testData.insert(post);
-  this.redirect('/');
-};
-
